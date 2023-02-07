6554bf61e760ce9daf46ce683ffaa19b10c6f726
Igor Boiko <igorboiko@gmail.com>
2023-02-07 16:28:03 +0200

    Add 03task 2solutions

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,44 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Знаходимо потрібний ul в розмітці:
+const imagesListRef = document.querySelector('.gallery');
+
+// Мінімальне оформлення списку
+imagesListRef.style.display = 'flex';
+imagesListRef.style.justifyContent = 'center';
+
+// Вар.1 Пишемо фукцію яка поверне нам елемент з потрібною розміткою: li>img з атрибутами src та alt з вхідного масиву
+const makeimagesListItem = ({ url, alt }) => {
+  const imageListItemRef = document.createElement('li');
+  imageListItemRef.insertAdjacentHTML('afterbegin', `<img src=${url} alt=${alt}>`);
+  // Мінімальне оформлення елементів списку
+  imageListItemRef.style.display = 'flex';
+  imageListItemRef.style.justifyContent = 'center';
+  imageListItemRef.style.width = '200px';
+  imageListItemRef.style.height = '200px';
+  imageListItemRef.style.margin = '5px';
+  imageListItemRef.style.overflow = 'hidden';
+  // Повертаємо результат з потрібною розміткою на місце виклику
+  return imageListItemRef;
+};
+// для всіх заданих об'єктів викличемо функцію
+const markupImagesInsert = images.map(makeimagesListItem);
+// // Додамо розмітку в список
+imagesListRef.append(...markupImagesInsert);
+
+// // Вар.2 з лекції:
+// // робимо один шаблонний рядок з масиву об'єктів: images.map() + .join("")
+// const imageListItemsRef = images.map(({ url, alt }) => `<li class="css"><img src=${url} alt=${alt}></li>`).join('');
+// // додаємо до вказаного списку через insertAdjacentHTML
+// imagesListRef.insertAdjacentHTML('afterbegin', imageListItemsRef);
+// // Оформлюємо через спільний клас
+// const imageListItems = imagesListRef.querySelectorAll('.css');
+// imageListItems.forEach(item => {
+//   item.style.display = 'flex';
+//   item.style.width = '200px';
+//   item.style.height = '200px';
+//   item.style.margin = '5px';
+//   item.style.overflow = 'hidden';
+// });
